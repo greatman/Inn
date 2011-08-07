1bc379cdcb8e18f375d37dcfcf23d6d9938cdd1c
Greatman <lebelwilliam@gmail.com>
2011-08-07 18:22:15 -0400

    - Implementing new way to include Register

--- src/main/java/me/greatman/plugins/inn/IPlayerListener.java
+++ src/main/java/me/greatman/plugins/inn/IPlayerListener.java
@@ -1,3 +1,26 @@
+/**
+ * 
+ * Copyright 2011 Greatman (https://github.com/greatman)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Some parts of this class (Line 46-77) is Copyright 2011 MilkBowl (http://github.com/MilkBowl)
+ * Modified by Greatman (http://github.com/greatman)
+ * 
+ */
+
 package me.greatman.plugins.inn;
 
 import org.bukkit.ChatColor;
